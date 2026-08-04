8aae7c590651d93933fcb14e5eb0dd98e09481ea
sudhanshu <sudhanshut58@gmail.com>
2026-08-04 21:37:23 +0530

    Improve login synchronization

--- PrismStructure/pages/LoginPage.js
+++ PrismStructure/pages/LoginPage.js
@@ -5,6 +5,7 @@ class LoginPage {
     this.emailInput = page.locator('[data-test="email"]');
     this.passwordInput = page.locator('[data-test="password"]');
     this.loginButton = page.locator('[data-test="login-submit"]');
+    this.signInNav = page.locator('[data-test="nav-sign-in"]');
   }
 
   async open() {
@@ -14,7 +15,17 @@ class LoginPage {
   async login(email, password) {
     await this.emailInput.fill(email);
     await this.passwordInput.fill(password);
+
+    const loginResponsePromise = this.page.waitForResponse(
+      (response) =>
+        response.url().includes('/users/login') &&
+        response.request().method() === 'POST' &&
+        response.status() === 200
+    );
+
     await this.loginButton.click();
+    await loginResponsePromise;
+    await this.signInNav.waitFor({ state: 'hidden' });
   }
 }
 
